4cd653cd1255c25c5dc72cd36d1965a8c65240b6
Matthew Sinnott <msinno01@mail.bbk.ac.uk>
2020-02-10 18:01:25 +0000

    initial commit

---
+++ ResDepth/solvate.py
@@ -0,0 +1,15 @@
+class Solvate:
+    def __init__(self,pdb_file):
+        self.pdb_file = pdb_file
+
+    def place_protein(self):
+        pass
+
+    def rotate_protein(self):
+        pass
+
+    def remove_protein_clash(self):
+        pass
+
+    def remove_non_bulk(self):
+        pass
